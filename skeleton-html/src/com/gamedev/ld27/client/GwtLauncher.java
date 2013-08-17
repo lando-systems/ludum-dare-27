56346b525a90c5f5fa34e0133672391a723b5b0e
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-08-17 06:46:17 +0000

    Initial commit of skeleton project

---
+++ skeleton-html/src/com/gamedev/ld27/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.gamedev.ld27.client;
+
+import com.gamedev.ld27.Skeleton;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Skeleton();
+	}
+}
