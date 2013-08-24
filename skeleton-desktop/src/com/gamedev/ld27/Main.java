705f6356f22714428132e8d87630a4d568054c83
bploeckelman <bploeckelman@7c682880-8db1-4081-97d4-4a7ec6be58f4>
2013-08-24 19:42:44 +0000

    Global config for consistent screen sizes between platforms

--- skeleton-desktop/src/com/gamedev/ld27/Main.java
+++ skeleton-desktop/src/com/gamedev/ld27/Main.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "skeleton";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.title = Config.title;
+		cfg.useGL20 = Config.useGL20;
+		cfg.width = Config.screenWidth;
+		cfg.height = Config.screenHeight;
 		
 		new LwjglApplication(new Skeleton(), cfg);
 	}
