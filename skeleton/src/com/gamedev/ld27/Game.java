8335d53d151e84a2d57866f6eec967d763bc5506
brossman <brossman@7c682880-8db1-4081-97d4-4a7ec6be58f4>
2013-08-24 23:26:04 +0000

    dialog box

---
+++ skeleton/src/com/gamedev/ld27/Game.java
@@ -0,0 +1,7 @@
+package com.gamedev.ld27;
+
+import com.gamedev.ld27.gameobjects.*;
+
+public class Game {
+	public static DialogBox DialogBox;
+}
--- skeleton/src/com/gamedev/ld27/gameobjects/DialogBox.java
+++ skeleton/src/com/gamedev/ld27/gameobjects/DialogBox.java
@@ -1,24 +1,51 @@
 package com.gamedev.ld27.gameobjects;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.gamedev.ld27.Config;
+import com.gamedev.ld27.Utils;
+import com.gamedev.ld27.Utils.EStringJustify;
 
 public class DialogBox extends GameObject {
+	
+	private final ArrayList<String> _dialogText = new ArrayList<String>(20);
+	private float _yPosition;
+	private int _maxText;
 
 	public DialogBox(Rectangle bounds) {
 		super(bounds);
+		_maxText = (int)(bounds.height / Config.textHeight);
 	}
 	
 	@Override
 	public void render(SpriteBatch batch) {
-		Fill(Color.BLUE);
+		Fill(Color.BLACK);
+		float x = _bounds.x + 10 - Config.screenHalfWidth;
+		float y = _yPosition - Config.screenHalfHeight;
+		
+		int i = Math.max(0,  _dialogText.size() - _maxText);		
+		for (; i < _dialogText.size(); i++) {
+			Utils.drawText(batch, _dialogText.get(i), x,  y, Color.RED, EStringJustify.LEFT);
+			y += Config.textHeight;
+		}	
 	}
 
 	@Override
 	public void update(float delta) {
-		// TODO Auto-generated method stub
-
+		// not using word wrap	
+		int textLines = Math.min(_maxText,  _dialogText.size());		
+		_yPosition = _bounds.y + (_bounds.height - (textLines * Config.textHeight)) /2;
 	}
-
+	
+	public void AppendText(String text) {
+		_dialogText.add(text);
+	}
+	
+	public void SetText(String text) {
+		_dialogText.clear();
+		_dialogText.add(text);
+	}
 }
