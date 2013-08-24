025bb478e03842b17a7ac8e2ff11e4ca87cf5157
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-08-24 17:28:05 +0000

    Added generic GameScreen class for easy switching between screens (title->menu->game->ending, etc...)

--- skeleton/src/com/gamedev/ld27/Skeleton.java
+++ skeleton/src/com/gamedev/ld27/Skeleton.java
@@ -1,66 +1,35 @@
 package com.gamedev.ld27;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Skeleton implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.gamedev.ld27.screens.GameScreen;
+import com.gamedev.ld27.screens.TitleScreen;
+
+public class Skeleton extends Game {
+
+	public Map<String, GameScreen> screens;
+
+	public Skeleton() {
+		super();
+		screens = new HashMap<String, GameScreen>();
+	}
+
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+	public void create() {
+		screens.put("Title", new TitleScreen(this));
+
+		setScreen(screens.get("Title"));
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		for (GameScreen screen : screens.values()) {
+			screen.dispose();
+		}
+		Gdx.app.exit();
 	}
 
-	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
-	}
 }
