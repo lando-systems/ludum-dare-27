4799d0163b671570bf6a7767ef612013cff3b774
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-08-25 05:05:35 +0000

    Moar items

--- skeleton/src/com/gamedev/ld27/items/offensive/BagOfKittens.java
+++ skeleton/src/com/gamedev/ld27/items/offensive/BagOfKittens.java
@@ -1,11 +1,12 @@
 package com.gamedev.ld27.items.offensive;
 
+import com.gamedev.ld27.Assets;
 import com.gamedev.ld27.items.OffensiveWeaponItem;
 
 public class BagOfKittens extends OffensiveWeaponItem {
 
 	public BagOfKittens() {
-		this.setDescription("Bag of Kittens");
-		
+		this.setDescription("Bag of Kittens: warning, dangerously cute");
+		this.setIcon(Assets.bagOfKittens);
 	}
 }
--- skeleton/src/com/gamedev/ld27/items/offensive/SillyString.java
+++ skeleton/src/com/gamedev/ld27/items/offensive/SillyString.java
@@ -1,13 +1,15 @@
 package com.gamedev.ld27.items.offensive;
 
+import com.gamedev.ld27.Assets;
 import com.gamedev.ld27.GameSettings;
 import com.gamedev.ld27.items.OffensiveWeaponItem;
 
 public class SillyString extends OffensiveWeaponItem {
 
 	public SillyString() {
-		this.setDescription("Can of silly String");
-
+		this.setDescription("Can of silly string: don't get it in your eyes");
+		this.setIcon(Assets.sillyString);
+		
 		setDefeats(GameSettings.CastleGuards);
 	}
 }
