73e8276adddcc2df65e8ee1419239cdfa66bd0c3
brossman <brossman@7c682880-8db1-4081-97d4-4a7ec6be58f4>
2013-08-25 10:47:26 +0000

    range weapons

--- skeleton/src/com/gamedev/ld27/items/OffensiveWeaponItem.java
+++ skeleton/src/com/gamedev/ld27/items/OffensiveWeaponItem.java
@@ -4,7 +4,6 @@
 package com.gamedev.ld27.items;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.gamedev.ld27.Assets;
 import com.gamedev.ld27.Game;
 
 public abstract class OffensiveWeaponItem extends BaseItem {
@@ -13,8 +12,9 @@ public abstract class OffensiveWeaponItem extends BaseItem {
 	protected float _animationTime = 1f;
 	private float _range = 1.5f;
 	private int _uses = 4;
-	private boolean _isMelee = true;	
-	private boolean _rotate = true;
+	private boolean _isMelee = true;
+	private TextureRegion _rangeImage;
+	private float _rangeScale = 0.5f;
 	
 	protected OffensiveWeaponItem(String name, String description) {
 		super(name, description);
@@ -34,7 +34,10 @@ public abstract class OffensiveWeaponItem extends BaseItem {
 	protected void setProperties(boolean melee, float tileRange, int uses, float speed)
 	{
 		_isMelee = melee;
-		_range = tileRange + 0.5f;
+		_range = tileRange;
+		if (melee) {
+			_range += 0.5f;
+		}
 		_uses = uses;
 		_animationTime = speed;		
 	}
@@ -57,7 +60,24 @@ public abstract class OffensiveWeaponItem extends BaseItem {
 		}
 	}
 	
+	protected void setRangeImage(TextureRegion texture, float scale) {
+		_rangeImage = texture;
+		_rangeScale = scale;
+	}
+	
 	public TextureRegion getWeaponUseImage(int direction) {
-		return getTexture();
+		return (_rangeImage != null) ? _rangeImage : getTexture();
 	}	
+	
+	public float getRangeScale() {
+		return _rangeScale;
+	}
+	
+	public float getRotation(int direction) {
+		return 0f;
+	}
+	
+	public boolean isSingleUse() {
+		return _isMelee;
+	}
 }
