b6556002251cd8f80709a6585a30f4479722fb75
clalande <clalande@7c682880-8db1-4081-97d4-4a7ec6be58f4>
2013-08-25 05:18:30 +0000

    some very basic advertisement stubs

--- skeleton/src/com/gamedev/ld27/items/BaseItem.java
+++ skeleton/src/com/gamedev/ld27/items/BaseItem.java
@@ -4,8 +4,14 @@
  */
 package com.gamedev.ld27.items;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.gamedev.ld27.BaseGamePlayObject;
+import com.gamedev.ld27.Config;
+import com.gamedev.ld27.Utils;
+import com.gamedev.ld27.Utils.EStringJustify;
 import com.gamedev.ld27.Game;
 
 public abstract class BaseItem extends BaseGamePlayObject {
@@ -18,6 +24,20 @@ public abstract class BaseItem extends BaseGamePlayObject {
 
 	protected void setDefeats(int defeats) {
 		_defeats = defeats;
+	}
+
+	/**
+	 * Render method that prints the icon followed by the text of the advertisement
+	 * @param batch
+	 * @param adBarBounds
+	 */
+	public void renderAd(SpriteBatch batch, Rectangle adBarBounds) {
+		icon.draw(batch);
+		
+		//Print the description text next to the icon
+		float x = adBarBounds.x + icon.getOriginX() - Config.screenHalfWidth + icon.getWidth();
+		float y = (adBarBounds.height -  adBarBounds.y)/2 - Config.screenHalfHeight;
+		Utils.drawText(batch, this.getAdDescription(), x,  y, Color.BLACK, EStringJustify.LEFT);
 	}
 
 	public void use() 
