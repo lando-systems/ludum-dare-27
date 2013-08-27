f758a6a58e1986a138f23a5c1e367a88a4944d62
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-08-27 00:39:09 +0000

    final commit before packaging and uploading

--- skeleton/src/com/gamedev/ld27/screens/PlayScreen.java
+++ skeleton/src/com/gamedev/ld27/screens/PlayScreen.java
@@ -58,7 +58,7 @@ public class PlayScreen extends GameScreen {
 		
 		Game.player = new Player(new Rectangle((leftWidth/2.0f -16), y + (mapHeight/2.0f)-16, 32, 32));
 		//TODO Adjust player speed here
-		Game.player.init(900,  10, 10);
+		Game.player.init(100,  10, 10);
 		_gameObjects.add(Game.player);
 		
 		y -= dialogHeight;
